fe4b82c4dd3ba7d658ce456a65b55aa40cc78331
monishaV17 <monishav643@gmail.com>
2025-08-22 19:57:12 +0530

    Update DBConnection.java

--- DB/DBConnection.java
+++ DB/DBConnection.java
@@ -2,12 +2,15 @@ package DB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBConnection{
-    public static Connection gConnection(){
+    public static Connection getConnection(){
         Connection con=null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Employee_DataBase","root","Monisha@123");
-        }
+            String url="jdbc:mysql://localhost:3306/Employee_DataBase";
+            String user="root";
+            String password=System.getenv("DB_PASSWORD");
+            con=DriverManager.getConnection(url, user, password);
+            }
         catch (Exception e){
             e.printStackTrace();
         }
